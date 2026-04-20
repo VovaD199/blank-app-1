b57c14a29ecff78396cdec3ded472a682c3adea3
VovaD <56256864+VovaD199@users.noreply.github.com>
2026-04-20 16:06:16 +0300

    Add Sales Dashboard title and user input feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,10 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.title("Sales Dashboard")
+name = st.text_input("User name")
+st.write(f"Hello, {name}")
+
 value = st.slider("Value", 0, 10, 5)
 st.write("Current value:", value)
