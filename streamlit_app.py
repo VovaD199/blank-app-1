85184cdbe1774f71178c550670144440678ffe85
VovaD <56256864+VovaD199@users.noreply.github.com>
2026-04-20 16:03:42 +0300

    Add slider to select a value in the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+value = st.slider("Value", 0, 10, 5)
+st.write("Current value:", value)
